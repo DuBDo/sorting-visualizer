44c2e22c95dcc6c336ebb140cb3d1f877001ae5a
DuBDo <146757620+DuBDo@users.noreply.github.com>
2023-12-21 06:49:42 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const n=30;
 const array=[];
 
+generateArray();
+
 //getting references to the buttons
 const bubbleBtn = document.getElementById("bubbleBtn");
 const selectionBtn = document.getElementById("selectionBtn");
@@ -15,8 +17,6 @@ insertionBtn.addEventListener("click", () => handleButtonClick(3));
 quickBtn.addEventListener("click", () => handleButtonClick(4));
 mergBtn.addEventListener("click", () => handleButtonClick(5));
 
-generateArray();
-
 function generateArray(){
     for(let i=0; i<n; i++){
         array[i]=Math.random();
@@ -24,7 +24,9 @@ function generateArray(){
     showBars(array);
 }
 
+
 function showBars(array){
+    const container = document.getElementById('container');
     container.innerHTML = " ";
 
     for(let i=0; i<array.length; i++){
@@ -38,15 +40,25 @@ function showBars(array){
 function handleButtonClick(buttonNumber){
     switch(buttonNumber){
         case 1:
+            bubbleBtn.style.backgroundColor= "Purple";
             bubbleSort(array);
             break;
         case 2:
+            selectionBtn.style.backgroundColor= "Purple";
             selectionSort(array);
             break;
         case 3:
+            insertionBtn.style.backgroundColor= "Purple";
             insertionSort(array);
+            break;
         case 4:
+            quickBtn.style.backgroundColor= "Purple";
             quickSort(0, array.length -1);
+            break;
+        case 5:
+            mergeBtn.style.backgroundColor= "Purple";
+            mergeSort(array);
+            break;
     }
 }
 
@@ -73,6 +85,8 @@ async function bubbleSort(array){
             document.querySelectorAll('.bar')[j + 1].style.backgroundColor = 'black';
         }
     }
+    //remove the background-Color of the button after fishing the sorting
+    bubbleBtn.style.backgroundColor = "white";
 }
 
 
@@ -111,6 +125,8 @@ async function selectionSort(array)
 
         showBars(array);
     }
+    //remove the background-Color of the button after fishing the sorting
+    selectionBtn.style.backgroundColor = "white";
 }
 
 //insertion sort
@@ -124,7 +140,7 @@ async function insertionSort(array) {
             // Red for comparison
             document.querySelectorAll(".bar")[j+1].style.backgroundColor = "red"; 
 
-            await new Promise(resolve => setTimeout(resolve, 10));
+            await new Promise(resolve => setTimeout(resolve, 100));
   
             // Blue for sorted elements
             document.querySelectorAll(".bar")[j+1].style.backgroundColor = "blue"; 
@@ -135,65 +151,48 @@ async function insertionSort(array) {
         showBars(array);
         await new Promise(resolve => setTimeout(resolve, 10));
     }
+    //remove the background-Color of the button after fishing the sorting
+    insertionBtn.style.backgroundColor = "white";
 }
   
 //quick sort
-async function swap(i, j){
-    [array[i], array[j]] = [array[j], array[i]];
+async function quickSort(array) {
+    await quickSortHelper(0, array.length - 1);
     showBars(array);
-    await sleep(500);
-    
-  }
-  
-  async function partition(low, high) {
-    const pivot = array[low];//making the first element pivot
-    let i = low + 1;
-    document.querySelector(".bar")[low].style.backgroundColor = "yellow";
-
-    for (let j = low+1; j <= high; j++) {
-      if (array[j] < pivot) {
-        i++;
-        //red for comparison
-        document.querySelectorAll(".bar")[j].style.backgroundColor = "yellow";
-
-        document.querySelectorAll(".bar")[i].style.backgroundColor = "red";
-        document.querySelectorAll(".bar")[j].style.backgroundColor = "red";
-
-
-        await swap(i, j);
-        document.querySelectorAll(".bar")[i].style.backgroundColor = "blue";
-        document.querySelectorAll(".bar")[j].style.backgroundColor = "blue";
-
-        i+=1;
-      }
-    }
-    //swap and change color to green for the pivot
-    
-
-    document.querySelectorAll(".bar")[low].style.backgroundColor = "red";
-    document.querySelectorAll(".bar")[i-1].style.backgroundColor = "red";
-    await swap(i-1, start);
-
-    for(let t=low; t<=i; t++){
-        document.querySelectorAll(".bar")[t].style.backgroundColor = "green";
-    }
-
-    return i - 1;
-  }
-
-  async function quickSort(low, high) {
-    if (low < high) {
-      const pivotIndex = await partition(low, high);
-      await Promise.all([
-        quickSort(low, pivotIndex - 1),
-        quickSort(pivotIndex + 1, high)
-      ]);
-    }
-  }
-
-function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-//mergsort
+async function quickSortHelper(low, high) {
+    if (low < high) {
+        let pivotIndex = await partition(low, high);
+        await Promise.all([
+            quickSortHelper(low, pivotIndex - 1),
+            quickSortHelper(pivotIndex + 1, high)
+        ]);
+    }
+}
 
+async function partition(low, high) {
+    let pivot = array[high];
+    let i = low - 1;
+
+    for (let j = low; j <= high - 1; j++) {
+        // Visualize comparison (blue color)
+        document.querySelectorAll(".bar")[j].style.backgroundColor = "blue"; 
+        showBars(array);
+        await new Promise(resolve => setTimeout(resolve, 50));
+
+        if (array[j] < pivot) {
+            i++;
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+
+        // Reset color after comparison
+        document.querySelectorAll(".bar")[j].style.backgroundColor = "black"; 
+        showBars(array);
+    }
+
+    [array[i+1], array[high]]= [array[high], array[i+1]];
+    return i + 1;
+}
+
+//merge sort
